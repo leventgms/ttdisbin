00abc508a9d5439d1b16bbf753ab7dc8ffc3019f
leventgms <103524450+leventgms@users.noreply.github.com>
2023-08-09 09:20:59 +0200

    Add files via upload

--- java/functions.js
+++ java/functions.js
@@ -1,1 +1,106 @@
-dfs
+
+$(document).ready(function(){
+    var Form1= document.getElementById("Form1");
+        var Form2= document.getElementById("Form2");
+        var Form3= document.getElementById("Form3");
+
+        var Next1= document.getElementById("Next1");
+        var Next2= document.getElementById("Next2");
+
+        Next1.onclick =function(){
+            Form1.style.left = "-1500px";
+            Form2.style.left = "0px";
+            progress.style.width= "146.66px"
+        };
+
+        Next2.onclick =function(){
+            Form2.style.left = "-1500px";
+            Form3.style.left = "0px";
+            progress.style.width= "219.99px"
+
+        };
+
+        Next3.onclick =function(){
+            Form3.style.left = "-1500px";
+            Form4.style.left = "0px";
+            progress.style.width= "293.32px"
+
+        };
+
+        Next4.onclick =function(){
+            Form4.style.left = "-1500px";
+            Form5.style.left = "0px";
+            progress.style.width= "366.65px"
+
+        };
+        Next5.onclick =function(){
+            Form5.style.left = "-1500px";
+            Form6.style.left = "0px";
+            progress.style.width= "439.98px"
+
+        };
+        Next6.onclick =function(){
+            Form6.style.left = "-1500px";
+            Form7.style.left = "0px";
+            progress.style.width= "513.31px"
+
+        };
+        Next7.onclick =function(){
+            Form7.style.left = "-1500px";
+            Form8.style.left = "0px";
+            progress.style.width= "586.64px"
+
+        };
+        Next8.onclick =function(){
+            Form8.style.left = "-1500px";
+            Form9.style.left = "0px";
+            progress.style.width= "659.97px"
+
+        };
+        Next9.onclick =function(){
+            Form9.style.left = "-1500px";
+            Form10.style.left = "0px";
+            progress.style.width= "733.30px"
+
+        };
+        Next10.onclick =function(){
+            Form10.style.left = "-1500px";
+            Form11.style.left = "0px";
+            progress.style.width= "806.63px"
+
+        };
+        Next11.onclick =function(){
+            Form11.style.left = "-1500px";
+            Form12.style.left = "0px";
+            progress.style.width= "879.96px"
+
+        };
+        
+        Next12.onclick =function(){
+            Form12.style.left = "-1500px";
+            Form13.style.left = "0px";
+            progress.style.width= "953.29px"
+
+        };
+        Next13.onclick =function(){
+            Form13.style.left = "-1500px";
+            Form14.style.left = "0px";
+            progress.style.width= "1026.62px"
+
+        };
+        Next14.onclick =function(){
+            Form14.style.left = "-1500px";
+            Form15.style.left = "0px";
+            progress.style.width= "1100px"
+
+        };
+
+
+});
+
+
+
+
+
+
+   
