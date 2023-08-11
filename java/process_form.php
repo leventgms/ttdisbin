dd8df64d34e1a22b7939e48d9814cbbdf4ff6088
leventgms <103524450+leventgms@users.noreply.github.com>
2023-08-11 11:47:47 +0200

    Update process_form.php

--- java/process_form.php
+++ java/process_form.php
@@ -48,6 +48,24 @@ $Bel_elicit_32m_13=$_POST["Bel_elicit_32m_13"];
 $Bel_elicit_32m_14=$_POST["Bel_elicit_32m_14"];
 $Bel_elicit_32m_15=$_POST["Bel_elicit_32m_15"];
 
+$Bel_elicit_333l_1=$_POST["Bel_elicit_333l_1"];
+$Bel_elicit_333l_2=$_POST["Bel_elicit_333l_2"];
+$Bel_elicit_333l_3=$_POST["Bel_elicit_333l_3"];
+$Bel_elicit_333l_4=$_POST["Bel_elicit_333l_4"];
+$Bel_elicit_333l_5=$_POST["Bel_elicit_333l_5"];
+$Bel_elicit_333l_6=$_POST["Bel_elicit_333l_6"];
+$Bel_elicit_333l_7=$_POST["Bel_elicit_333l_7"];
+$Bel_elicit_333l_8=$_POST["Bel_elicit_333l_8"];
+$Bel_elicit_333l_9=$_POST["Bel_elicit_333l_9"];
+$Bel_elicit_333l_10=$_POST["Bel_elicit_333l_10"];
+$Bel_elicit_333l_11=$_POST["Bel_elicit_333l_11"];
+$Bel_elicit_333l_12=$_POST["Bel_elicit_333l_12"];
+$Bel_elicit_333l_13=$_POST["Bel_elicit_333l_13"];
+$Bel_elicit_333l_14=$_POST["Bel_elicit_333l_14"];
+$Bel_elicit_333l_15=$_POST["Bel_elicit_333l_15"];
+$Bel_elicit_333l_16=$_POST["Bel_elicit_333l_16"];
+$Bel_elicit_333l_17=$_POST["Bel_elicit_333l_17"];
+
 $host="ec2-3-248-141-201.eu-west-1.compute.amazonaws.com";
 $dbname="d43sg2k0mcvm00";
 $username="ylnkdtgmqzhkfu";
@@ -71,9 +89,13 @@ $sql = "INSERT INTO ttdisbin (Bel_elicit_30_1, Bel_elicit_30_2,
                               Bel_elicit_32l_15, Bel_elicit_32m_1, Bel_elicit_32m_2, Bel_elicit_32m_3, Bel_elicit_32m_4,
                               Bel_elicit_32m_5, Bel_elicit_32m_6, Bel_elicit_32m_7, Bel_elicit_32m_8, Bel_elicit_32m_9,
                               Bel_elicit_32m_10, Bel_elicit_32m_11, Bel_elicit_32m_12, Bel_elicit_32m_13, Bel_elicit_32m_14, 
-                              Bel_elicit_32m_15)
+                              Bel_elicit_32m_15, Bel_elicit_333l_1, Bel_elicit_333l_2,
+                              Bel_elicit_333l_3, Bel_elicit_333l_4, Bel_elicit_333l_5, Bel_elicit_333l_6, Bel_elicit_333l_7,
+                              Bel_elicit_333l_8, Bel_elicit_333l_9, Bel_elicit_333l_10, Bel_elicit_333l_11, Bel_elicit_333l_12,
+                              Bel_elicit_333l_13, Bel_elicit_333l_14, Bel_elicit_333l_15)
         VALUES ($1,	$2,	$3, 	$4, 	$5, 	$6, 	$7, 	$8, 	$9, 	$10,    $11,    $12,    $13,	$14,	$15,    $16,    $17,    $18,    $19,    $20,    $21,    $22,    $23,    $24,
-        $25,	$26,	$27,	$28,	$29,	$30,	$31,	$32,    $33,	$34,	$35,	$36,	$37,	$38,	$39,	$40,	$41,	$42,	$43,	$44,	$45)";                      
+        $25,	$26,	$27,	$28,	$29,	$30,	$31,	$32,    $33,	$34,	$35,	$36,	$37,	$38,	$39,	$40,	$41,	$42,	$43,	$44,	$45,    $46,	$47,	$48,
+        $49,	$50,	$51,	$52,	$53,	$54,	$55,	$56,	$57,    $58,	$59,	$60)";                      
 
 $stmt = pg_prepare($conn, "insert_stmt", $sql);
 
@@ -81,7 +103,9 @@ $params = array( $Bel_elicit_30_1, $Bel_elicit_30_2, $Bel_elicit_30_3, $Bel_elic
     $Bel_elicit_30_11, $Bel_elicit_30_12, $Bel_elicit_30_13, $Bel_elicit_30_14, $Bel_elicit_30_15, $Bel_elicit_32l_1, $Bel_elicit_32l_2, $Bel_elicit_32l_3, $Bel_elicit_32l_4,
     $Bel_elicit_32l_5, $Bel_elicit_32l_6, $Bel_elicit_32l_7, $Bel_elicit_32l_8, $Bel_elicit_32l_9, $Bel_elicit_32l_10, $Bel_elicit_32l_11, $Bel_elicit_32l_12, $Bel_elicit_32l_13, $Bel_elicit_32l_14,
     $Bel_elicit_32l_15, $Bel_elicit_32m_1, $Bel_elicit_32m_2, $Bel_elicit_32m_3, $Bel_elicit_32m_4, $Bel_elicit_32m_5, $Bel_elicit_32m_6, $Bel_elicit_32m_7, $Bel_elicit_32m_8,
-    $Bel_elicit_32m_9, $Bel_elicit_32m_10, $Bel_elicit_32m_11, $Bel_elicit_32m_12, $Bel_elicit_32m_13, $Bel_elicit_32m_14, $Bel_elicit_32m_15);
+    $Bel_elicit_32m_9, $Bel_elicit_32m_10, $Bel_elicit_32m_11, $Bel_elicit_32m_12, $Bel_elicit_32m_13, $Bel_elicit_32m_14, $Bel_elicit_32m_15, $Bel_elicit_333l_1, $Bel_elicit_333l_2,
+    $Bel_elicit_333l_3, $Bel_elicit_333l_4, $Bel_elicit_333l_5, $Bel_elicit_333l_6, $Bel_elicit_333l_7, $Bel_elicit_333l_8, $Bel_elicit_333l_9, $Bel_elicit_333l_10, $Bel_elicit_333l_11, $Bel_elicit_333l_12,
+    $Bel_elicit_333l_13, $Bel_elicit_333l_14, $Bel_elicit_333l_15);
 
 $result = pg_execute($conn, "insert_stmt", $params);
 ?>
