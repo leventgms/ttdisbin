f71f0da1284dad912b2fac1f11051c2fea449684
leventgms <103524450+leventgms@users.noreply.github.com>
2023-08-11 09:48:18 +0200

    Update shuffle.js

--- java/shuffle.js
+++ java/shuffle.js
@@ -1,4 +1,5 @@
-const formId=["Form1","Form2","Form3","Form4","Form5","Form6","Form7","Form8","Form9","Form10","Form11","Form12","Form13","Form14","Form15"];
+const formId=["Form1","Form2","Form3","Form4","Form5","Form6","Form7","Form8","Form9","Form10","Form11","Form12","Form13","Form14","Form15", "Form16","Form17",
+              "Form18","Form19","Form20","Form21","Form22","Form23","Form24","Form25","Form26","Form27","Form28","Form29","Form30"];
 
 function shuffle(array){
     var currentIndex= array.length;
