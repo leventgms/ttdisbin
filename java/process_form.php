094cfe0cee9da38c9398351ac4f1cbce9d5f7953
leventgms <103524450+leventgms@users.noreply.github.com>
2023-08-14 11:37:37 +0200

    belief done

--- java/process_form.php
+++ java/process_form.php
@@ -128,6 +128,73 @@ $Bel_elicit_23l_13=$_POST["Bel_elicit_23l_13"];
 $Bel_elicit_23l_14=$_POST["Bel_elicit_23l_14"];
 $Bel_elicit_23l_15=$_POST["Bel_elicit_23l_15"];
 
+$Bel_elicit_23m_1=$_POST["Bel_elicit_23m_1"];
+$Bel_elicit_23m_2=$_POST["Bel_elicit_23m_2"];
+$Bel_elicit_23m_3=$_POST["Bel_elicit_23m_3"];
+$Bel_elicit_23m_4=$_POST["Bel_elicit_23m_4"];
+$Bel_elicit_23m_5=$_POST["Bel_elicit_23m_5"];
+$Bel_elicit_23m_6=$_POST["Bel_elicit_23m_6"];
+$Bel_elicit_23m_7=$_POST["Bel_elicit_23m_7"];
+$Bel_elicit_23m_8=$_POST["Bel_elicit_23m_8"];
+$Bel_elicit_23m_9=$_POST["Bel_elicit_23m_9"];
+$Bel_elicit_23m_10=$_POST["Bel_elicit_23m_10"];
+$Bel_elicit_23m_11=$_POST["Bel_elicit_23m_11"];
+$Bel_elicit_23m_12=$_POST["Bel_elicit_23m_12"];
+$Bel_elicit_23m_13=$_POST["Bel_elicit_23m_13"];
+$Bel_elicit_23m_14=$_POST["Bel_elicit_23m_14"];
+$Bel_elicit_23m_15=$_POST["Bel_elicit_23m_15"];
+
+
+$Bel_elicit_03_1=$_POST["Bel_elicit_03_1"];
+$Bel_elicit_03_2=$_POST["Bel_elicit_03_2"];
+$Bel_elicit_03_3=$_POST["Bel_elicit_03_3"];
+$Bel_elicit_03_4=$_POST["Bel_elicit_03_4"];
+$Bel_elicit_03_5=$_POST["Bel_elicit_03_5"];
+$Bel_elicit_03_6=$_POST["Bel_elicit_03_6"];
+$Bel_elicit_03_7=$_POST["Bel_elicit_03_7"];
+$Bel_elicit_03_8=$_POST["Bel_elicit_03_8"];
+$Bel_elicit_03_9=$_POST["Bel_elicit_03_9"];
+$Bel_elicit_03_10=$_POST["Bel_elicit_03_10"];
+$Bel_elicit_03_11=$_POST["Bel_elicit_03_11"];
+$Bel_elicit_03_12=$_POST["Bel_elicit_03_12"];
+$Bel_elicit_03_13=$_POST["Bel_elicit_03_13"];
+$Bel_elicit_03_14=$_POST["Bel_elicit_03_14"];
+$Bel_elicit_03_15=$_POST["Bel_elicit_03_15"];
+
+$Bel_elicit_333_l_1=$_POST["Bel_elicit_333_l_1"];
+$Bel_elicit_333_l_2=$_POST["Bel_elicit_333_l_2"];
+$Bel_elicit_333_l_3=$_POST["Bel_elicit_333_l_3"];
+$Bel_elicit_333_l_4=$_POST["Bel_elicit_333_l_4"];
+$Bel_elicit_333_l_5=$_POST["Bel_elicit_333_l_5"];
+$Bel_elicit_333_l_6=$_POST["Bel_elicit_333_l_6"];
+$Bel_elicit_333_l_7=$_POST["Bel_elicit_333_l_7"];
+$Bel_elicit_333_l_8=$_POST["Bel_elicit_333_l_8"];
+$Bel_elicit_333_l_9=$_POST["Bel_elicit_333_l_9"];
+$Bel_elicit_333_l_10=$_POST["Bel_elicit_333_l_10"];
+$Bel_elicit_333_l_11=$_POST["Bel_elicit_333_l_11"];
+$Bel_elicit_333_l_12=$_POST["Bel_elicit_333_l_12"];
+$Bel_elicit_333_l_13=$_POST["Bel_elicit_333_l_13"];
+$Bel_elicit_333_l_14=$_POST["Bel_elicit_333_l_14"];
+$Bel_elicit_333_l_15=$_POST["Bel_elicit_333_l_15"];
+
+
+$Bel_elicit_333_m_1=$_POST["Bel_elicit_333_m_1"];
+$Bel_elicit_333_m_2=$_POST["Bel_elicit_333_m_2"];
+$Bel_elicit_333_m_3=$_POST["Bel_elicit_333_m_3"];
+$Bel_elicit_333_m_4=$_POST["Bel_elicit_333_m_4"];
+$Bel_elicit_333_m_5=$_POST["Bel_elicit_333_m_5"];
+$Bel_elicit_333_m_6=$_POST["Bel_elicit_333_m_6"];
+$Bel_elicit_333_m_7=$_POST["Bel_elicit_333_m_7"];
+$Bel_elicit_333_m_8=$_POST["Bel_elicit_333_m_8"];
+$Bel_elicit_333_m_9=$_POST["Bel_elicit_333_m_9"];
+$Bel_elicit_333_m_10=$_POST["Bel_elicit_333_m_10"];
+$Bel_elicit_333_m_11=$_POST["Bel_elicit_333_m_11"];
+$Bel_elicit_333_m_12=$_POST["Bel_elicit_333_m_12"];
+$Bel_elicit_333_m_13=$_POST["Bel_elicit_333_m_13"];
+$Bel_elicit_333_m_14=$_POST["Bel_elicit_333_m_14"];
+$Bel_elicit_333_m_15=$_POST["Bel_elicit_333_m_15"];
+
+
 
 $host="ec2-3-248-141-201.eu-west-1.compute.amazonaws.com";
 $dbname="d43sg2k0mcvm00";
@@ -165,12 +232,22 @@ $sql = "INSERT INTO ttdisbin (Bel_elicit_30_1, Bel_elicit_30_2,
                               Bel_elicit_3_3m_5, Bel_elicit_3_3m_6, Bel_elicit_3_3m_7, Bel_elicit_3_3m_8, Bel_elicit_3_3m_9, Bel_elicit_3_3m_10, Bel_elicit_3_3m_11,
                               Bel_elicit_3_3m_12, Bel_elicit_3_3m_13, Bel_elicit_3_3m_14, Bel_elicit_3_3m_15, Bel_elicit_23l_1,
                               Bel_elicit_23l_2, Bel_elicit_23l_3, Bel_elicit_23l_4, Bel_elicit_23l_5, Bel_elicit_23l_6, Bel_elicit_23l_7, Bel_elicit_23l_8, Bel_elicit_23l_9,
-                              Bel_elicit_23l_10, Bel_elicit_23l_11, Bel_elicit_23l_12, Bel_elicit_23l_13, Bel_elicit_23l_14, Bel_elicit_23l_15)
+                              Bel_elicit_23l_10, Bel_elicit_23l_11, Bel_elicit_23l_12, Bel_elicit_23l_13, Bel_elicit_23l_14, Bel_elicit_23l_15, Bel_elicit_23m_1, Bel_elicit_23m_2, Bel_elicit_23m_3,
+                              Bel_elicit_23m_4, Bel_elicit_23m_5, Bel_elicit_23m_6, Bel_elicit_23m_7, Bel_elicit_23m_8, Bel_elicit_23m_9, Bel_elicit_23m_10, Bel_elicit_23m_11, Bel_elicit_23m_12, 
+                              Bel_elicit_23m_13, Bel_elicit_23m_14, Bel_elicit_23m_15, Bel_elicit_03_1, Bel_elicit_03_2, Bel_elicit_03_3, Bel_elicit_03_4, Bel_elicit_03_5, Bel_elicit_03_6,
+                              Bel_elicit_03_7, Bel_elicit_03_8, Bel_elicit_03_9, Bel_elicit_03_10, Bel_elicit_03_11, Bel_elicit_03_12, Bel_elicit_03_13, Bel_elicit_03_14, Bel_elicit_03_15, 
+                              Bel_elicit_333_l_1, Bel_elicit_333_l_2, Bel_elicit_333_l_3, Bel_elicit_333_l_4, Bel_elicit_333_l_5, Bel_elicit_333_l_6, Bel_elicit_333_l_7, Bel_elicit_333_l_8,
+                              Bel_elicit_333_l_9, Bel_elicit_333_l_10, Bel_elicit_333_l_11, Bel_elicit_333_l_12, Bel_elicit_333_l_13, Bel_elicit_333_l_14, Bel_elicit_333_l_15, Bel_elicit_333_m_1,
+                              Bel_elicit_333_m_2, Bel_elicit_333_m_3, Bel_elicit_333_m_4, Bel_elicit_333_m_5, Bel_elicit_333_m_6, Bel_elicit_333_m_7, Bel_elicit_333_m_8, Bel_elicit_333_m_9,
+                            Bel_elicit_333_m_10, Bel_elicit_333_m_11, Bel_elicit_333_m_12, Bel_elicit_333_m_13, Bel_elicit_333_m_14, Bel_elicit_333_m_15)
         VALUES ($1,	$2,	$3, 	$4, 	$5, 	$6, 	$7, 	$8, 	$9, 	$10,    $11,    $12,    $13,	$14,	$15,    $16,    $17,    $18,    $19,    $20,    $21,    $22,    $23,    $24,
         $25,	$26,	$27,	$28,	$29,	$30,	$31,	$32,    $33,	$34,	$35,	$36,	$37,	$38,	$39,	$40,	$41,	$42,	$43,	$44,	$45,    $46,	$47,	$48,
         $49,	$50,	$51,	$52,	$53,	$54,	$55,	$56,	$57,    $58,	$59,	$60,    $61,	$62,	$63,	$64,	$65,	$66,	$67,	$68,	$69,	$70,	$71,	$72,
         $73,	$74,	$75,	$76,	$77,	$78,	$79,	$80,	$81,    $82,	$83,	$84,	$85,	$86,	$87,	$88,	$89,	$90,    $91,	$92,	$93,	$94,	$95,	$96,
-        $97,	$98,	$99,	$100,	$101,	$102,	$103,	$104,	$105,   $106,	$107,	$108,	$109,	$110,	$111,  $112,   $113,   $114,	$115,	$116,	$117,	$118,	$119,	$120)";                      
+        $97,	$98,	$99,	$100,	$101,	$102,	$103,	$104,	$105,   $106,	$107,	$108,	$109,	$110,	$111,  $112,   $113,   $114,	$115,	$116,	$117,	$118,	$119,	$120,
+        $121,	$122,	$123,	$124,	$125,	$126,	$127,	$128,	$129,   $130,	$131,	$132,	$133,	$134,	$135,	$136,	$137,	$138,	$139,	$140,	$141,	$142,	$143,	$144,
+        $145,   $146,	$147,	$148,	$149,	$150,	$151,	$152,	$153,   $154,	$155,	$156,	$157,	$158,	$159,	$160,	$161,	$162,	$163,	$164,	$165,	$166,	$167,	$168,
+        $169,	$170,	$171,   $172,	$173,	$174,	$175,	$176,	$177,   $178,	$179,	$180)";                      
 
 $stmt = pg_prepare($conn, "insert_stmt", $sql);
 
@@ -187,7 +264,13 @@ $params = array( $Bel_elicit_30_1, $Bel_elicit_30_2, $Bel_elicit_30_3, $Bel_elic
     $Bel_elicit_3_3m_2, $Bel_elicit_3_3m_3, $Bel_elicit_3_3m_4, $Bel_elicit_3_3m_5, $Bel_elicit_3_3m_6, $Bel_elicit_3_3m_7, $Bel_elicit_3_3m_8, $Bel_elicit_3_3m_9, $Bel_elicit_3_3m_10, $Bel_elicit_3_3m_11,
     $Bel_elicit_3_3m_12, $Bel_elicit_3_3m_13, $Bel_elicit_3_3m_14, $Bel_elicit_3_3m_15, $Bel_elicit_23l_1, $Bel_elicit_23l_2, $Bel_elicit_23l_3, $Bel_elicit_23l_4,
     $Bel_elicit_23l_5, $Bel_elicit_23l_6, $Bel_elicit_23l_7, $Bel_elicit_23l_8, $Bel_elicit_23l_9, $Bel_elicit_23l_10, $Bel_elicit_23l_11, $Bel_elicit_23l_12, $Bel_elicit_23l_13, $Bel_elicit_23l_14,
-    $Bel_elicit_23l_15);
+    $Bel_elicit_23l_15, $Bel_elicit_23m_1, $Bel_elicit_23m_2, $Bel_elicit_23m_3, $Bel_elicit_23m_4, $Bel_elicit_23m_5, $Bel_elicit_23m_6, $Bel_elicit_23m_7, $Bel_elicit_23m_8,
+    $Bel_elicit_23m_9, $Bel_elicit_23m_10, $Bel_elicit_23m_11, $Bel_elicit_23m_12, $Bel_elicit_23m_13, $Bel_elicit_23m_14, $Bel_elicit_23m_15, $Bel_elicit_03_1, $Bel_elicit_03_2,
+    $Bel_elicit_03_3, $Bel_elicit_03_4, $Bel_elicit_03_5, $Bel_elicit_03_6, $Bel_elicit_03_7, $Bel_elicit_03_8, $Bel_elicit_03_9, $Bel_elicit_03_10, $Bel_elicit_03_11, $Bel_elicit_03_12,
+    $Bel_elicit_03_13, $Bel_elicit_03_14, $Bel_elicit_03_15, $Bel_elicit_333_l_1, $Bel_elicit_333_l_2, $Bel_elicit_333_l_3, $Bel_elicit_333_l_4, $Bel_elicit_333_l_5, $Bel_elicit_333_l_6, $Bel_elicit_333_l_7, $Bel_elicit_333_l_8,
+    $Bel_elicit_333_l_9, $Bel_elicit_333_l_10, $Bel_elicit_333_l_11, $Bel_elicit_333_l_12, $Bel_elicit_333_l_13, $Bel_elicit_333_l_14, $Bel_elicit_333_l_15, $Bel_elicit_333_m_1,
+    $Bel_elicit_333_m_2, $Bel_elicit_333_m_3, $Bel_elicit_333_m_4, $Bel_elicit_333_m_5, $Bel_elicit_333_m_6, $Bel_elicit_333_m_7, $Bel_elicit_333_m_8, $Bel_elicit_333_m_9, $Bel_elicit_333_m_10,
+    $Bel_elicit_333_m_11, $Bel_elicit_333_m_12, $Bel_elicit_333_m_13, $Bel_elicit_333_m_14, $Bel_elicit_333_m_15);
 
 $result = pg_execute($conn, "insert_stmt", $params);
 ?>
