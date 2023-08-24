0e728970ad73fed769aff38faad2841fc80eddd4
leventgms <103524450+leventgms@users.noreply.github.com>
2023-08-24 15:47:20 +0200

    update

--- java/functions.js
+++ java/functions.js
@@ -743,9 +743,9 @@ $(document).ready(function () {
     welcome.onclick = function () {
         Instructions.style.left = "-1500px";
         var nextformId = storedform[0];
-        var nextform = document.getElementById("instructions_ineq");
+        var nextform = document.getElementById(nextformId);
         nextform.style.left = "0px";
-        adjustChoiceAreaHeight("instructions_ineq");
+        adjustChoiceAreaHeight(nextformId);
 
     };
 
