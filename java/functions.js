4680390efcbc6aac77017c3d6878e135260aa590
leventgms <103524450+leventgms@users.noreply.github.com>
2023-08-24 15:02:13 +0200

    update

--- java/functions.js
+++ java/functions.js
@@ -741,9 +741,9 @@ $(document).ready(function () {
     welcome.onclick = function () {
         Instructions.style.left = "-1500px";
         var nextformId = storedform[0];
-        var nextform = document.getElementById("instructions_trust");
+        var nextform = document.getElementById("instructions_util");
         nextform.style.left = "0px";
-        adjustChoiceAreaHeight("instructions_trust");
+        adjustChoiceAreaHeight("instructions_util");
 
     };
 
