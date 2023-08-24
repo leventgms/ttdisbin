6592826ec74858b1192546cf312719f3a124ea22
leventgms <103524450+leventgms@users.noreply.github.com>
2023-08-24 15:30:51 +0200

    update

--- java/functions.js
+++ java/functions.js
@@ -722,7 +722,8 @@ $(document).ready(function () {
             choiceareaContainer.style.height = '2070px';
             choiceareaContainer.style.width = '1200px';
         } else if (contentId === 'instructions_util'){
-            choiceareaContainer.style.height = '1550px';
+            choiceareaContainer.style.height = '1650px';
+            choiceareaContainer.style.width = '1200px';
         } else if (contentId === 'instructions_ineq'){
             choiceareaContainer.style.height = '1750px';
         } else if (contentId === 'instructions_risk'){
